709449022a38a0a0b1f825656aa98a1660277356
Ifty Test <iftyi695@gmail.com>
2024-08-14 17:17:17 +0000

    Initial commit

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -12,7 +12,7 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 };
 
 async function main() {
-  const replace_contractAddress = "reboot";
+  const replace_contractAddress = "o";
   const [signer] = await hre.ethers.getSigners();
 
   const replace_contractFactory = await hre.ethers.getContractFactory("PERC20Sample");
